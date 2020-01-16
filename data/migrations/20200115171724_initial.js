e75bcba54da3e90ac80f0c1efe5d86a20e793f01
Ruth Poliakon <r.poliakon@gmail.com>
2020-01-15 17:24:12 -0700

    index setup and initial data migration

--- data/migrations/20200115171724_initial.js
+++ data/migrations/20200115171724_initial.js
@@ -1,8 +1,14 @@
-
-exports.up = function(knex) {
-  
+exports.up = async knex => {
+  await knex.schema.createTable("users", users => {
+    users.increments();
+    users
+      .string("username", 128)
+      .notNullable()
+      .unique();
+    users.string("password", 128).notNullable();
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = async knex => {
+  await knex.schema.dropTableIfExists("users");
 };
---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const helmet = require("helmet");
+
+const server = express();
+const port = process.env.PORT || 4000;
+
+server.use(helmet());
+server.use(express.json());
+
+server.get("/", (req, res, next) => {
+  res.json({
+    message: "Welcome to our API"
+  });
+});
+
+server.use((req, res) => {
+  res.status(404).json({
+    message: "Route was not found"
+  });
+});
+
+server.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({
+    message: "An internal error occurred, please try again later"
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`);
+});
